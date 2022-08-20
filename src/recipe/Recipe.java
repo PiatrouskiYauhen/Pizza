2a4f889b7b3b966e2c60e9bc04e9fffdf285e3e5
SviatlanaYatsuk <bkbb@bk.ru>
2022-08-20 09:34:17 +0300

    add costOfRecipe

--- src/recipe/Recipe.java
+++ src/recipe/Recipe.java
@@ -4,19 +4,24 @@ import java.util.Map;
 import java.util.UUID;
 
 public class Recipe {
-    private String ID;
+    private String idRecipe;
     private String nameRecipe;
     private Map<String, Integer> mapOfRecipe;
+    private Integer costOfRecipe;
+
+    public String getIdRecipe() {return idRecipe;}
+    public String getNameRecipe() {return nameRecipe;}
+    public Map<String, Integer> getMapOfRecipe() {return mapOfRecipe;}
+    public Integer getPriceOfRecipe() {return costOfRecipe;}
 
     public Recipe(String nameRecipe) {
-        this.ID = UUID.randomUUID().toString();
+        this.idRecipe = UUID.randomUUID().toString();
         this.nameRecipe = nameRecipe;
-        //здесь будет МАП <Наименование ингридиента, вес>, когда будет склад сов семи ингридиентами
+
+        //здесь будет МАП <Имя_ингридиента, вес>, когда будет склад со в семи ингридиентами
+        //this.costOfRecipe = "метод формирования стоимости рецепта"
     }
-//здесь будет метод формирования стоимости рецепта
 
-
-
-
+    //здесь будет метод формирования стоимости рецепта, после того, как у нас появится МАП <Имя_ингридиента, вес>
 
 }
