1ae3031b6bd3aa182e4eec55ac64d70ec3cd51df
SviatlanaYatsuk <bkbb@bk.ru>
2022-08-27 13:54:17 +0300

    change1.In class Recipe: d tipe of fild cost, added ьуеmethod costOfPizza; 2.Copied IngredientStorage class from Alexander pull request

--- src/recipe/Recipe.java
+++ src/recipe/Recipe.java
@@ -1,5 +1,8 @@
 package recipe;
 
+import ingredient.IngredientStorage;
+
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -7,19 +10,21 @@ public class Recipe {
     private String id;
     private String name;
     private Map<String, Integer> ingredientMap;//
-    private Integer cost;
+    private Double cost;
 
     public String getId() {
         return id;
     }
+
     public String getName() {
         return name;
     }
+
     public Map<String, Integer> getIngredientMap() {
         return ingredientMap;
     }
 
-    public Integer getCost() {
+    public Double getCost() {
         return cost;
     }
 
@@ -27,5 +32,18 @@ public class Recipe {
         this.id = UUID.randomUUID().toString();
         this.name = nameRecipe;
         this.ingredientMap = ingredientMap;
+        this.cost = costOfpizza(this);
+    }
+
+    public Double costOfpizza(Recipe recipe) {
+
+        List<String> listKeys = recipe.ingredientMap.keySet().stream().toList();
+        Double sumOfCosts = 0.0;
+        for (int i = 0; i < listKeys.size(); i++) {
+            Double costOfIngredientOnStorage = IngredientStorage.getData().get(i).getPrice();//цена ингридиента на складе (за какой вес???)
+            Double cosеOfIngredientInRecipe = ingredientMap.get(i) * costOfIngredientOnStorage;// = вес ингридиента * стоимость ингредиента на складе
+            sumOfCosts = sumOfCosts + cosеOfIngredientInRecipe;
+        }
+        return sumOfCosts;
     }
 }
