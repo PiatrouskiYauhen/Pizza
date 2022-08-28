a14db7f2f73a416cda1b4f9ad030e2e7f760cbb1
RomaSapon <rsapon@mail.ru>
2022-08-28 16:38:55 +0300

    add in enum new obgect milliliter and add method ratioOfConversion()

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
-import ingredient.Ingredient;
-import ingredient.IngredientGenerator;
-import ingredient.IngredientStorage;
-
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("********Welcome to the best pizza store!********");
