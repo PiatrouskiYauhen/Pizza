28156eb1332d1b7620eae18aadf27cdf5fceb8d1
RomaSapon <rsapon@mail.ru>
2022-08-28 16:38:55 +0300

    add in enum new obgect milliliter and add method ratioOfConversion()

--- src/ingredient/Measure.java
+++ src/ingredient/Measure.java
@@ -1,14 +1,98 @@
 package ingredient;
 
 public enum Measure {
-    GRAM("g."),
-    KILOGRAM("kg."),
-    LITER("l."),
-    PIECE("p.");    //штуки
+    GRAM("g.") {
+        public double ratioOfConversion(Measure measure) {
+            if (measure == null) {
+                return 0d;
+            }
+            double returnValue = 0d;
+            switch (measure) {
+                case KILOGRAM,LITER: {
+                    returnValue = 1000d;
+                    break;
+                }
+                case GRAM,PIECE,MILLILITER: {
+                    returnValue = 1d;
+                    break;
+                }
+            }
+            return returnValue;
+        }
+    },
+    KILOGRAM("kg.") {
+        public double ratioOfConversion(Measure measure) {
+            if (measure == null) {
+                return 0d;
+            }
+            double returnValue = 0d;
+            switch (measure) {
+                case KILOGRAM,PIECE,LITER: {
+                    returnValue = 1d;
+                    break;
+                }
+                case GRAM,MILLILITER: {
+                    returnValue = 0.001d;
+                    break;
+                }
+            }
+            return returnValue;
+        }
+    },
+    LITER("l.") {
+        public double ratioOfConversion(Measure measure) {
+            if (measure == null) {
+                return 0d;
+            }
+            double returnValue = 0d;
+            switch (measure) {
+                case KILOGRAM,PIECE,LITER: {
+                    returnValue = 1d;
+                    break;
+                }
+                case GRAM,MILLILITER: {
+                    returnValue = 0.001d;
+                    break;
+                }
+            }
+            return returnValue;
+        }
+    },
+    PIECE("p.") {
+                public double ratioOfConversion(Measure measure) {
+                    if (measure == null) {
+                        return 0d;
+                    }
+                    return 1d;
+                }
+            },    //штуки
+    MILLILITER("ml.") {
+        public double ratioOfConversion(Measure measure) {
+            if (measure == null) {
+                return 0d;
+            }
+            double returnValue = 0d;
+            switch (measure) {
+                case KILOGRAM,LITER: {
+                    returnValue = 1000d;
+                    break;
+                }
+                case GRAM,PIECE,MILLILITER: {
+                    returnValue = 1d;
+                    break;
+                }
+            }
+            return returnValue;
+        }
+    };
 
     final String name;
 
     Measure(String name) {
         this.name = name;
     }
+
+    //Метод принимает параметр measure - объект перечисления Measure, к которой будет конвертироваться текущая мера ингридиента.
+    //Метод возвращает коэффициент корвертации между текущей мерой ингридиента и мерой, переданой в параметре.
+    public abstract double ratioOfConversion(Measure measure);
 }
