5e3bb05f299104f2cb150337cecfb30158a75599
galkha <galkha25@gmail.com>
2022-08-29 21:26:49 +0300

    pizza-13 Add subtractIngredient, changed weight in quantity

--- src/ingredient/IngredientService.java
+++ src/ingredient/IngredientService.java
@@ -4,19 +4,37 @@ import java.util.HashMap;
 
 public class IngredientService {
 
-    public static void addNewIngredient(Ingredient ingredient, int weight, double price) {
+    public static void addIngredient(Ingredient ingredient, int quantity, double price) throws Exception {
+        validateNatural(quantity);
         HashMap<String, Ingredient> storage = IngredientStorage.getData();
         storage.compute(ingredient.getId(), (key, ingr) -> {
             Ingredient updated;
             if (ingr == null) {
                 updated = ingredient;
-                updated.setWeight(weight);
+                updated.setWeight(quantity);
             } else {
                 updated = ingr;
-                updated.setWeight(ingr.getWeight() + weight);
+                updated.setWeight(ingr.getWeight() + quantity);
             }
             updated.setPrice(price);
             return updated;
         });
     }
+
+    public static void subtractIngredient(String ingredientId, int quantity) throws Exception {
+        validateNatural(quantity);
+        Ingredient ingredient = IngredientStorage.getById(ingredientId);
+        int subtract = ingredient.getWeight() - quantity;
+        if (subtract < 0) {
+            throw new Exception("There is no such amount of " + ingredient.getName() + " in storage");
+        }
+        ingredient.setWeight(subtract);
+    }
+
+    public static void validateNatural(int quantity) throws Exception {
+        if (quantity < 0) {
+            throw new Exception("Enter a natural number");
+        }
+    }
+
 }
