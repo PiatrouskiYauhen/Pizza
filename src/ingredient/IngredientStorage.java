6e28c26c30a0c46275a3168ce017361312ea098b
SviatlanaYatsuk <107875544+SviatlanaYatsuk@users.noreply.github.com>
2022-08-29 20:39:46 +0300

    pizza-15 counting recipe price (#22)

--- src/ingredient/IngredientStorage.java
+++ src/ingredient/IngredientStorage.java
@@ -1,3 +1,4 @@
+
 package ingredient;
 
 import java.util.HashMap;
--- src/recipe/Recipe.java
+++ src/recipe/Recipe.java
@@ -1,5 +1,8 @@
 package recipe;
 
+import ingredient.IngredientStorage;
+
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -7,25 +10,40 @@ public class Recipe {
     private String id;
     private String name;
     private Map<String, Integer> ingredientMap;//
-    private Integer cost;
+    private Double cost;
 
     public String getId() {
         return id;
     }
+
     public String getName() {
         return name;
     }
+
     public Map<String, Integer> getIngredientMap() {
         return ingredientMap;
     }
 
-    public Integer getCost() {
+    public Double getCost() {
         return cost;
     }
 
-    public Recipe(String nameRecipe, Map<String, Integer> ingredientMap) {
+    public Recipe(String name, Map<String, Integer> ingredientMap) throws Exception {
         this.id = UUID.randomUUID().toString();
-        this.name = nameRecipe;
+        this.name = name;
         this.ingredientMap = ingredientMap;
+        this.cost = costOfPizza();
+    }
+
+    private Double costOfPizza() throws Exception {
+
+        List<String> listKeys = this.ingredientMap.keySet().stream().toList();
+        Double sumOfCosts = 0.0;
+        for (int i = 0; i < listKeys.size(); i++) {
+            Double costOfIngredientOnStorage = IngredientStorage.getById(listKeys.get(i)).getPrice();//цена ингридиента на складе (за какой вес???)
+            Double costOfIngredientInRecipe = ingredientMap.get(listKeys.get(i)) * costOfIngredientOnStorage;// = вес ингридиента * стоимость ингредиента на складе
+            sumOfCosts += costOfIngredientInRecipe;
+        }
+        return sumOfCosts;
     }
 }
