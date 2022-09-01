4cad086990bed5546eb30c7d0e6e67f6364b2f43
galkha <galkha25@gmail.com>
2022-09-01 21:35:30 +0300

    pizza-20

--- src/recipe/RecipeService.java
+++ src/recipe/RecipeService.java
@@ -1,4 +1,15 @@
 package recipe;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RecipeService {
+    public void recipeIsReadyValidation(Recipe recipe){
+        Map<String,Integer> ingredientsOfRecipe = recipe.getIngredientMap();
+        ingredientsOfRecipe.entrySet().stream(entry -> {
+
+        });
+
+
+    }
 }
